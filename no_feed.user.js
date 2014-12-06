8fe191029008961121cccc87a414dcde9483461e
Giovanni Charles <giovanni.charles@gmail.com>
2014-12-06 10:31:47 +0000

    no feed script

---
+++ no_feed.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        no feed
+// @namespace   mm
+// @description takes away the feed
+// @include     https://www.facebook.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.getElementById("stream_pagelet").lastChild.innerHTML = "Nah"
